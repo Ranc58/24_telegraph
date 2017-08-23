9da8119b5fdccf397a52aa38dcf5c0d6fcb168cd
Ranc58 <Unseen82>
2017-08-23 13:43:53 +0300

    Change redirect for story view page after edit. Fix for correct edit page, add button for confirm changes. Add readme.

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Telegraph Clone
 
-{TODO. There will be project description}
+Clone of [telegra.ph](http://telegra.ph/).\
+On this site user create anonymous story and publish it, with the subsequent possibility of post editing.\
+Post editing using cookies authorization and possible within 7 days.
+
+Working example: [telegraph-clone.herokuapp.com](https://telegraph-clone.herokuapp.com/)
+
+# How to install
+1. Recomended use venv or virtualenv for better isolation.\
+   Venv setup example: \
+   `python3 -m venv myenv`\
+   `source myenv/bin/activate`
+2. Install requirements: \
+   `pip3 install -r requirements.txt` (alternatively try add `sudo` before command)
+
+# How to launch
+   - Run server `gunicorn server:app`
+   - Open on browser `http://127.0.0.1:8000`
 
 # Project Goals
 
--- server.py
+++ server.py
@@ -5,11 +5,9 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 from models import Story, db
 import uuid
 
-
 SIMPLE_CHARS = string.ascii_letters + string.digits
 MAX_URL_LEN = 8
 MAX_COOKIES_AGE = 60 * 60 * 24 * 7
-
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -21,6 +19,15 @@ if not os.path.exists('stories.db'):
 
 def get_random_url():
     return ''.join(random.choice(SIMPLE_CHARS) for char in range(MAX_URL_LEN))
+
+
+def update_story(edited_story):
+    stories = db.session.query(Story)
+    story_to_update = stories.filter_by(story_url=edited_story['story_url'])
+    for key, value in edited_story.items():
+        if value is not '':
+            story_to_update.update({key: value})
+    db.session.commit()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -55,19 +62,18 @@ def view_story(story_url):
 @app.route('/edit/<story_url>', methods=['GET', 'POST'])
 def edit_story(story_url):
     story_uid = request.cookies.get('story_uid')
-    stories = db.session.query(Story)
-    story_to_update = stories.filter_by(story_url=story_url)
-    story_page = stories.get_or_404(story_url)
+    story_page = db.session.query(Story).get_or_404(story_url)
     if story_uid == story_page.story_uid:
         if request.method == 'POST':
             story_title = request.form.get('header')
             story_signature = request.form.get('signature')
             story_body = request.form.get('body')
-            story_to_update.update({'story_title': story_title,
-                                    'story_signature': story_signature,
-                                    'story_body': story_body})
-            db.session.commit()
-        return render_template('form.html', story=story_page) # TODO Change to story_view
+            edited_story = {'story_title': story_title,
+                            'story_signature': story_signature,
+                            'story_body': story_body,
+                            'story_url': story_page.story_url}
+            update_story(edited_story)
+        return render_template('form.html', story=story_page)
     else:
         return "You don't have permissions for edit this story!"
 
