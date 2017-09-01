ca5e8c80f38df2df0757cb25bb3d395921147c82
Ranc58 <rancvova@gmail.com>
2017-09-01 21:59:36 +0300

    edit some db column names and add new column author_id for cookies. edit templates for new db column vars, fix None values for forms. fix cookies distribution(before every story getting new cookie). del edited story filter decorator for unnecessary.

--- models.py
+++ models.py
@@ -4,8 +4,9 @@ db = SQLAlchemy()
 
 
 class Story(db.Model):
-    story_url = db.Column(db.String(32), primary_key=True)
+    story_slug = db.Column(db.String(32), primary_key=True)
     story_uid = db.Column(db.String(128))
     story_title = db.Column(db.String(64))
     story_signature = db.Column(db.String(128))
     story_body = db.Column(db.Text)
+    author_id = db.Column(db.String(64))
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import os
 import random
 import string
 import uuid
-from functools import wraps
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from models import Story, db
 
@@ -19,26 +18,13 @@ if not os.path.exists('stories.db'):
     db.create_all()
 
 
-def get_random_url():
+def get_random_slug():
     return ''.join(random.choice(SIMPLE_CHARS) for char in range(MAX_URL_LEN))
 
 
-def filter_story_rows(func):
-    @wraps(func)
-    # Filter for only edited rows in story
-    def wrapper(edited_story):
-        value_in_row = 1
-        changed_rows = dict(filter(lambda key:
-                                   key[value_in_row],
-                                   edited_story.items()))
-        func(changed_rows)
-    return wrapper
-
-
-@filter_story_rows
 def update_story(edited_story):
     stories = db.session.query(Story)
-    story_to_update = stories.filter_by(story_url=edited_story['story_url'])
+    story_to_update = stories.filter_by(story_slug=edited_story['story_slug'])
     story_to_update.update(edited_story)
     db.session.commit()
 
@@ -46,37 +32,41 @@ def update_story(edited_story):
 @app.route('/', methods=['GET', 'POST'])
 def add_new_story():
     if request.method == 'POST':
+        author_id = request.cookies.get('cookie')
+        if not author_id:
+            author_id = str(uuid.uuid4())
         story_uid = str(uuid.uuid4())
         story_title = request.form.get('header')
         story_signature = request.form.get('signature')
         story_body = request.form.get('body')
-        story_url = get_random_url()
-        db.session.add(Story(story_url=story_url,
+        story_slug = get_random_slug()
+        db.session.add(Story(story_slug=story_slug,
                              story_title=story_title,
                              story_signature=story_signature,
                              story_body=story_body,
-                             story_uid=story_uid))
+                             story_uid=story_uid,
+                             author_id=author_id))
         db.session.commit()
         new_story = make_response(redirect(url_for('view_story',
-                                                   story_url=story_url)))
-        new_story.set_cookie('story_uid', story_uid, max_age=MAX_COOKIES_AGE)
+                                                   story_slug=story_slug)))
+        new_story.set_cookie('author_id', author_id, max_age=MAX_COOKIES_AGE)
         return new_story
     else:
         return render_template('form.html')
 
 
-@app.route('/<story_url>')
-def view_story(story_url):
-    story_uid = request.cookies.get('story_uid')
-    story = db.session.query(Story).get_or_404(story_url)
-    return render_template('story.html', story=story, story_uid=story_uid)
+@app.route('/<story_slug>')
+def view_story(story_slug):
+    author_id = request.cookies.get('author_id')
+    story = db.session.query(Story).get_or_404(story_slug)
+    return render_template('story.html', story=story, author_id=author_id)
 
 
-@app.route('/edit/<story_url>', methods=['GET', 'POST'])
-def edit_story(story_url):
-    story_uid = request.cookies.get('story_uid')
-    story_page = db.session.query(Story).get_or_404(story_url)
-    if story_uid == story_page.story_uid:
+@app.route('/edit/<story_slug>', methods=['GET', 'POST'])
+def edit_story(story_slug):
+    author_id = request.cookies.get('author_id')
+    story_page = db.session.query(Story).get_or_404(story_slug)
+    if author_id == story_page.author_id:
         if request.method == 'POST':
             story_title = request.form.get('header')
             story_signature = request.form.get('signature')
@@ -84,7 +74,7 @@ def edit_story(story_url):
             edited_story = {'story_title': story_title,
                             'story_signature': story_signature,
                             'story_body': story_body,
-                            'story_url': story_page.story_url}
+                            'story_slug': story_page.story_slug}
             update_story(edited_story)
         return render_template('form.html', story=story_page)
     else:
